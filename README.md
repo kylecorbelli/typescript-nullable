14ac6dbe41870c5aa6a4a0e764fe05374a7fef05
Kyle Corbelli <kyle.corbelli@gmail.com>
2018-05-09 13:13:54 -0700

    Circle and codecov

---
+++ README.md
@@ -0,0 +1,5 @@
+# TypeScript Nullable
+
+[![CircleCI](https://circleci.com/gh/kylecorbelli/typescript-nullable.svg?style=shield)](https://circleci.com/gh/kylecorbelli/typescript-nullable)
+
+[![codecov](https://codecov.io/gh/kylecorbelli/typescript-nullable/branch/master/graph/badge.svg)](https://codecov.io/gh/kylecorbelli/typescript-nullable)
--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,24 @@ export type None = null | undefined
 
 export type Nullable<T> = T | None
 
-function isNone<T> (nullable: Nullable<T>): boolean {
+function nullableIsNone<T> (nullable: Nullable<T>): boolean {
   return nullable === null || nullable === undefined
 }
 
 function nullableMap<A, B> (func: (val: A) => B, nullable: Nullable<A>): Nullable<B> {
-  return isNone(nullable)
+  return nullableIsNone(nullable)
     ? null
     : func(nullable as A)
 }
 
 function nullableWithDefault<T> (defaultVal: T, nullable: Nullable<T>): T {
-  return isNone(nullable)
+  return nullableIsNone(nullable)
     ? defaultVal
     : nullable as T
 }
 
 function nullableAndThen<A, B> (func: (val: A) => Nullable<B>, nullable: Nullable<A>): Nullable<B> {
-  return isNone(nullable)
+  return nullableIsNone(nullable)
     ? null
     : func(nullable as A)
 }
@@ -30,7 +30,7 @@ function nullableAp<A, B> (
   applicativeNullable: Nullable<(val: A) => B>,
   targetNullable: Nullable<A>,
 ): Nullable<B> {
-  return isNone(applicativeNullable) || isNone(targetNullable)
+  return nullableIsNone(applicativeNullable) || nullableIsNone(targetNullable)
     ? null
     : (applicativeNullable as (val: A) => B)(targetNullable as A)
 }
@@ -38,6 +38,7 @@ function nullableAp<A, B> (
 export const Nullable = {
   andThen: curry(nullableAndThen),
   ap: curry(nullableAp),
+  isNone: nullableIsNone,
   map: curry(nullableMap),
   withDefault: curry(nullableWithDefault),
 }
